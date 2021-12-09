580b5d39604a17723ed1c7f6baaf65e115a918b4
erinchristoph <90008641+erinchristoph@users.noreply.github.com>
2021-12-09 17:51:49 -0500

    Final added

--- jquery/functions.js
+++ jquery/functions.js
@@ -9,3 +9,32 @@ $( function() {
     }
   });
 } );
+
+
+
+
+document.onkeydown = detectKey;
+var boundaries = {
+  left: 0;
+}
+function detectKey(e) {
+    var posLeft = document.getElementById('circle').offsetLeft;
+    var posTop = document.getElementById('circle').offsetTop;
+    e = e || window.event;
+    if (e.keyCode == '38') {
+        // up arrow
+        document.getElementById('circle').style.marginTop  = (posTop-25)+"px";
+    }
+    else if (e.keyCode == '40') {
+        // down arrow
+        document.getElementById('circle').style.marginTop  = (posTop+10)+"px";
+    }
+    else if (e.keyCode == '37') {
+       // left arrow
+        document.getElementById('circle').style.marginLeft  = (posLeft-25)+"px";
+    }
+    else if (e.keyCode == '39') {
+       // right arrow
+        document.getElementById('circle').style.marginLeft  = (posLeft+10)+"px";
+    }
+}
---
+++ testfinal/script.js
@@ -0,0 +1,88 @@
+const canvas = document.getElementById('canvas1');
+const ctx = canvas.getContext('2d');
+const CANVAS_WIDTH = canvas.width = 1200;
+const CANVAS_HEIGHT = canvas.height = 800;
+
+const keys = [];
+
+const player = {
+  x: 400,
+  y: 400,
+  width: 150,
+  height: 270,
+  frameX: 0,
+  frameY: 1,
+  speed: 25,
+  moving: false
+}
+
+const playerSprite = new Image();
+playerSprite.src = 'newsprite.png';
+const backgroundLayer1 = new Image();
+backgroundLayer1.src = 'joyfulwork.png';
+
+function drawSprite(img, sX, sY, sW, sH, dX, dY, dW, dH){
+  ctx.drawImage(img, sX, sY, sW, sH, dX, dY, dW, dH);
+}
+
+window.addEventListener("keydown", function(e) {
+  keys[e.keyCode] = true;
+  player.moving = true;
+});
+
+window.addEventListener("keyup", function(e) {
+  delete keys[e.keyCode];
+  player.moving = false;
+});
+
+function movePlayer() {
+  if (keys[38] && player.y > 10) {
+    player.y -= player.speed;
+    player.frameY = 3;
+    player.moving = true;
+  }
+  if (keys[37] && player.x > 10) {
+    player.x -= player.speed;
+    player.frameY = 1;
+    player.moving = true;
+  }
+  if (keys[40] && player.y < canvas.height - player.height) {
+    player.y += player.speed;
+    player.frameY = 0;
+    player.moving = true;
+  }
+  if (keys[39] && player.x < canvas.width - player.width) {
+    player.x += player.speed;
+    player.frameY = 2;
+    player.moving = true;
+  }
+}
+
+function handlePlayerFrame(){
+  if (player.frameX < 3 && player.moving) player.frameX++
+  else player.frameX = 0;
+}
+
+let fps, fpsInterval, startTime, now, then, elapsed;
+
+function startAnimating(fps){
+  fpsInterval = 1000/fps;
+  then = Date.now();
+  startTime = then;
+  animate();
+}
+
+function animate(){
+  requestAnimationFrame(animate);
+  now = Date.now();
+  elapsed = now - then;
+  if (elapsed > fpsInterval){
+    then = now - (elapsed % fpsInterval);
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.drawImage(backgroundLayer1, 0,0);
+    drawSprite(playerSprite, player.width * player.frameX, player.height * player.frameY, player.width, player.height, player.x, player.y, player.width, player.height);
+    movePlayer();
+    handlePlayerFrame();
+  }
+}
+startAnimating(8);
